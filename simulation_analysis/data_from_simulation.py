746ac1333bcdbfa3202b71367d3a6c321c1a761a
niktoris1 <dmitriy.v.sirotkin@gmail.com>
2022-01-31 12:44:44 +0300

    Started data preparation

--- simulation_analysis/data_from_simulation.py
+++ simulation_analysis/data_from_simulation.py
@@ -24,4 +24,21 @@ for line in read_tsv:
 tree_class_tree = MakeTreeClassTree(newick_file_name,
                                     dict_name_muts = dict_name_muts, dict_name_times = None, dict_name_types = None, data_type = 1)
 
+samples_for_usher = []
+
+for node in tree_class_tree.all_nodes():
+    if node.data.type == 'Sample':
+        samples_for_usher.append(node)
+
+samples_for_usher_to_csv = []
+for sample in samples_for_usher:
+    pass
+
+f = open('/Users/LAB-SCG-125/Documents/Fitness_data/test/test.csv', 'w', newline='')
+
+writer = csv.writer(f)
+writer.writerow(['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT'])
+
+f.close()
+
 tree_class_tree.show()
